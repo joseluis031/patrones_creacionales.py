891f285ccaab75085115b478d139d42d0b03ad05
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-11-08 17:23:30 +0100

    por fin

--- Ejercicio1.1/samur.py
+++ Ejercicio1.1/samur.py
@@ -127,21 +127,99 @@ plt.show()
 #quiero que a la columna de Hora Intervencion le restes la columna de Hora Solicitud y me pases el resultado a minutos
 
 import datetime as dt
-
+import numpy as np
 data['Hora Intervención'] = pd.to_datetime(data['Hora Intervención'])
 data['Hora Solicitud'] = pd.to_datetime(data['Hora Solicitud'])
 
 data['Tiempo de respuesta'] = data['Hora Intervención'] - data['Hora Solicitud']
 
+data['Tiempo de respuesta'] = np.where(data['Hora Intervención']< data['Hora Solicitud'], data['Tiempo de respuesta'] + pd.Timedelta(days=1), data['Tiempo de respuesta'])
+#corrigeme para que tenga en cuenta cuando es media noche
+
+
 data['Tiempo de respuesta'] = data['Tiempo de respuesta'].dt.total_seconds() / 60  # Convierte a minutos
 
-print(data.head())
-
+#print(data.head())
+data = data.dropna()
 data.to_csv('Ejercicio1.1/datoslimpios_tiempo_respuesta.csv', sep=';', encoding='UTF-8')
 
+media = data.groupby('Distrito')['Tiempo de respuesta'].mean()
+print(media)
+
+#quiero un histograma con la columna de Tiempo de respuesta
+
+import matplotlib.pyplot as plt
+
+#hazme un grafico de barras de esa media con plt
+
+media.plot(kind='bar', figsize=(10,5))
+plt.show()
+
+'''
+
+#quiero ver datos nulos de columnas
+
+#print(data.isnull().sum())  # Mostrar el número de valores nulos en cada columna
+
+#quitar filas con datos nulos en Hora Intervencion y Hora Solicitud
+
+datos = data.dropna(subset=['Hora Intervención', 'Hora Solicitud', 'Código', 'Distrito'])
+
+#quitar columna hospital
+
+datos = datos.drop(['Hospital'], axis=1)
+#print(datos.isnull().sum())  # Mostrar el número de valores nulos en cada columna
+
+'''
+'''
+import pandas as pd
+
+# Lee tu archivo CSV
+df = pd.read_csv('Ejercicio1.1/datoslimpios.csv', sep=';', encoding='UTF-8')
+
+import pandas as pd
+
+# Lee tu archivo CSV
+from datetime import datetime# Función para calcular la diferencia entre las horas
+def calcular_diferencia(hora_solicitud, hora_intervencion):
+    formato = '%H:%M:%S'
+    solicitud = datetime.strptime(hora_solicitud, formato)
+    intervencion = datetime.strptime(hora_intervencion, formato)
+
+    # Asegura que la hora de intervención sea posterior o igual a la hora de solicitud
+    if intervencion < solicitud:
+        intervencion += pd.DateOffset(days=1)
+
+    # Calcula la diferencia en minutos
+    diferencia = (intervencion - solicitud).total_seconds() / 60
+    return diferencia
+
+# Aplica la función para calcular la diferencia en minutos
+df['Tiempo de Respuesta'] = df.apply(lambda row: calcular_diferencia(row['Hora Solicitud'], row['Hora Intervención']), axis=1)
+
+# Guarda el DataFrame actualizado en un nuevo archivo CSV
+df.to_csv('datoslimpios_tiemporespt.csv', index=False)
+
+#quitame todas las filas en las que el Distrito sea LEGANES
 
 
 
 
 
 
+media = df.groupby('Distrito')['Tiempo de Respuesta'].mean()
+
+#424
+
+print(media)
+
+#quiero un histograma con la columna de Tiempo de respuesta
+
+import matplotlib.pyplot as plt
+
+#hazme un grafico de barras de esa media con plt
+
+media.plot(kind='bar', figsize=(40,5))
+plt.show()
+
+'''
